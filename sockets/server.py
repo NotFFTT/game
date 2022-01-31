b246613a80bbfa5d9152c112a0e2d66e23e7f877
David Hecker <heckerdw@gmail.com>
2022-01-31 11:56:40 -0800

    Server handles multi client

--- sockets/server.py
+++ sockets/server.py
@@ -12,23 +12,34 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind(ADDRESS)
 
+player_1_x_y = None
+player_2_x_y = None
 
-def handle_client(connection, address):
+def handle_client(connection, address, player_name):
     connected = True
-
+    
     while connected:
         message_length = connection.recv(HEADER).decode(FORMAT)
         print('Message length: ', message_length)
         if message_length:
             msg_len = int(message_length)
             msg = connection.recv(msg_len).decode(FORMAT)
-            print(msg)
+            
+            if player_name == 1:
+                player_1_x_y = msg
+                print('PLAYER ONE', player_1_x_y)
+
+            else:
+                player_2_x_y = msg
+                print('PLAYER TWO', player_2_x_y)
+
     connection.close()
 
 def start():
     server.listen(4)
     print("Waiting for players to connect...")
     
+    player_name = 1
     while True:
         connection, address = server.accept()
         # player_name = connection.recv(1024).decode()
@@ -36,7 +47,8 @@ def start():
 
         # connection.send(bytes(("Welcome to the game! ", FORMAT)))
         
-        thread = threading.Thread(target=handle_client, args=(connection, address))
+        thread = threading.Thread(target=handle_client, args=(connection, address, player_name))
+        player_name += 1
         thread.start()
 
         print('new connection ', connection)
